5dd39149c0dd9f0c65270b09de6751bb3a04752c
Soroush Alinia <soroushalinia.wm@gmail.com>
2022-01-23 15:54:03 +0330

    Add 'todo' model

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+import models
+from database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
---
+++ src/models.py
@@ -0,0 +1,14 @@
+from sqlite3 import Date
+from xmlrpc.client import Boolean
+from sqlalchemy import Column, DateTime, Integer, String, Boolean
+
+from database import Base
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    task_name = Column(String)
+    task_date = Column(DateTime)
+    task_done = Column(Boolean, default=False)
